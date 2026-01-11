ef4242d45aa83816a533a9ded6bf7816f875290c
Wahei's MacBook Air <fujisakiwahei@gmail.com>
2026-01-11 16:45:40 +0900

    microCMSの環境変数をネストしたオブジェクトに整理

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,8 +9,10 @@ export default defineNuxtConfig({
   // 環境変数をクライアント側（ブラウザ）でも使えるように設定
   runtimeConfig: {
     public: {
-      microcmsServiceDomain: process.env.NUXT_PUBLIC_MICROCMS_SERVICE_DOMAIN || process.env.MICROCMS_SERVICE_DOMAIN,
-      microcmsApiKey: process.env.NUXT_PUBLIC_MICROCMS_API_KEY || process.env.MICROCMS_API_KEY,
+      microCMS: {
+        serviceDomain: process.env.NUXT_PUBLIC_MICROCMS_SERVICE_DOMAIN || process.env.MICROCMS_SERVICE_DOMAIN,
+        apiKey: process.env.NUXT_PUBLIC_MICROCMS_API_KEY || process.env.MICROCMS_API_KEY,
+      },
     },
   },
 
