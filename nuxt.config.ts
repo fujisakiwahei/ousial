26e0693037d3dc92585a133cc990d1079e95745e
Wahei's MacBook Air <fujisakiwahei@gmail.com>
2026-01-12 22:23:53 +0900

    Add favicon and Apple Touch Icon to nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,6 +23,11 @@ export default defineNuxtConfig({
           rel: "stylesheet",
           href: "https://fonts.googleapis.com/css2?family=Cormorant+SC:wght@400;700&family=Noto+Serif+JP:wght@400;700&family=Shippori+Mincho:wght@400;700&display=swap",
         },
+        { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+        // Apple Touch Icon (iOSホーム画面用)
+        { rel: "apple-touch-icon", sizes: "180x180", href: "/apple-touch-icon.png" },
+        // (任意) 高解像度用PNG
+        { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon-32x32.png" },
       ],
     },
   },
